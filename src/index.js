d4717eb3ef0ba33aca04d51572bd406b487d8353
Umair Saleem <saleemakhtar35302@gmail.com>
2023-09-30 22:58:15 +0500

    setup express server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function hello(username) {
-  console.log(`Hello, ${username}`);
+const { app } = require('./app');
+
+const PORT = 8000;
+
+// fake DB connection
+const connectToDB = () => {
+  return Promise.resolve();
 };
+
+const startServer = async () => {
+  // establish DB connection here
+  await connectToDB();
+
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT} 🚀`));
+};
+
+startServer().catch((err) => {
+  console.error(err instanceof Error ? err.message : err);
+});
