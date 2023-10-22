cbaff43282996105532aec90b4968bfc0bc5c6e2
Umair Saleem <saleemakhtar35302@gmail.com>
2023-10-22 21:26:34 +0500

    refactor test setup

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
   // An array of regexp pattern strings used to skip coverage collection
@@ -21,4 +19,5 @@ module.exports = {
   setupFilesAfterEnv: ['./src/test-setup.js'],
   // The glob patterns Jest uses to detect test files
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  verbose: true,
 };
--- src/__tests__/app.spec.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const { app } = require('../app');
-
-describe('App', () => {
-  it('returns 200 status', async () => {
-    const response = await request(app).get('/api').send();
-    expect(response.statusCode).toBe(200);
-  });
-});
