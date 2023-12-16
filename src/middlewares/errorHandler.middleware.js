68d843d00cdba001d7429bb4007e103ff0bff283
Umair Saleem <saleemakhtar35302@gmail.com>
2023-12-16 23:26:12 +0500

    refactor errorHandler middleware

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -2,15 +2,21 @@ const { logger } = require('../config');
 
 // eslint-disable-next-line no-unused-vars
 const errorHandlerMiddleware = (err, req, res, next) => {
-  logger.error(err.message);
   const statusCode = err.statusCode || 500;
+  const type = err.name || 'Internal Server Error';
+  const message = err.message || 'Something went wrong';
+  const path = req.path || '';
+  const location = err.location || '';
+
+  logger.error(message, { path, location });
+
   res.status(statusCode).json({
     errors: [
       {
-        type: err.name,
-        message: err.message,
-        path: '',
-        location: '',
+        type,
+        message,
+        path,
+        location,
       },
     ],
   });
